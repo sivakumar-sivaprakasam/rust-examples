3c763a085cbabc7c0af155aa0c2d789ce0c745f1
madlyrust <s.sivakumar77@hotmail.com>
2023-01-15 08:35:15 +0800

    removed target

--- format/target/debug/deps/format-13b4897c99b855dd.d
+++
@@ -1,5 +0,0 @@
-c:\rust-examples\format\target\debug\deps\format-13b4897c99b855dd.rmeta: src\main.rs
-
-c:\rust-examples\format\target\debug\deps\format-13b4897c99b855dd.d: src\main.rs
-
-src\main.rs:
