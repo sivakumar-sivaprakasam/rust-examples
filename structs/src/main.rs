cf5c1ee4c5b835713bace91dde94705f4a97cf2a
madlyrust <s.sivakumar77@hotmail.com>
2023-01-15 16:31:51 +0800

    With Struct examples

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,40 @@
+use std::fmt::{self, Display};
+
 fn main() {
-    println!("Hello, world!");
+    let e: Employee = Employee::new("John".to_string(), 30, "HR".to_string());
+    println!("{}", e);
+    println!("{:?}", e);
+    println!("{:#?}", e);
+}
+
+struct Employee {
+    name: String,
+    age: i32,
+    dept: String,
+}
+
+impl Employee {
+    pub fn new(name: String, age: i32, dept: String) -> Self {
+        Self { name, age, dept }
+    }
+}
+
+impl fmt::Debug for Employee {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Employee")
+            .field("Name", &self.name)
+            .field("Age", &self.age)
+            .field("Department", &self.dept)
+            .finish()
+    }
+}
+
+impl Display for Employee {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Employee [Name: {}, Age: {}, Department: {}]",
+            self.name, self.age, self.dept
+        )
+    }
 }
