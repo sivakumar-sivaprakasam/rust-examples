f56a29d743dd6aa66a9ac32f330d5cc7a4397494
madlyrust <s.sivakumar77@hotmail.com>
2023-01-15 16:32:36 +0800

    removed target

--- structs/target/debug/deps/structs-87aabf5e9d9acd8c.d
+++
@@ -1,5 +0,0 @@
-c:\rust-examples\structs\target\debug\deps\structs-87aabf5e9d9acd8c.rmeta: src\main.rs
-
-c:\rust-examples\structs\target\debug\deps\structs-87aabf5e9d9acd8c.d: src\main.rs
-
-src\main.rs:
