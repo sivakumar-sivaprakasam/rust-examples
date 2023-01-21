974e01ff161a6f949c0b2f48649a418fa680bd1b
madlyrust <s.sivakumar77@hotmail.com>
2023-01-21 11:51:44 +0800

    Deleted target

--- traits/target/debug/deps/traits-8e6160fc7fcd28f5.d
+++
@@ -1,5 +0,0 @@
-c:\rust-examples\traits\target\debug\deps\traits-8e6160fc7fcd28f5.rmeta: src\main.rs
-
-c:\rust-examples\traits\target\debug\deps\traits-8e6160fc7fcd28f5.d: src\main.rs
-
-src\main.rs:
--- traits/target/debug/deps/traits-a89f721437cb9e0c.d
+++
@@ -1,5 +0,0 @@
-c:\rust-examples\traits\target\debug\deps\traits-a89f721437cb9e0c.rmeta: src\main.rs
-
-c:\rust-examples\traits\target\debug\deps\traits-a89f721437cb9e0c.d: src\main.rs
-
-src\main.rs:
