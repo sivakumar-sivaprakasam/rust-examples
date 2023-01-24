0e9e246ad201325ca9b3df899e6366b8e9747eb0
madlyrust <s.sivakumar77@hotmail.com>
2023-01-24 14:02:24 +0800

    Enum examples

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,34 @@
+#![allow(dead_code)]
 fn main() {
-    println!("Hello, world!");
+    let a = Ack::ExactlyOnce;
+    println!("Received acknowledge {a:#?}");
+
+    let b = EnhancedAck::ExactlyOnce;
+    println!("Received Acknowledge: {b:?}");
+
+    let c = EnhancedAck::AtMostOnce(String::from("Hello Rust"));
+    println!("Received Acknowledge: {c:?}");
+    if let EnhancedAck::AtMostOnce(c1) = c {
+        println!("Extracted data from Enum --> Message: {}", c1);
+    }
+
+    let d = EnhancedAck::AtLeastOnce(100, String::from("Hello Rust Again"));
+    println!("Received Acknowledge: {d:?}");
+    if let EnhancedAck::AtLeastOnce(d1, d2) = d {
+        println!("Extracted data from Enum --> Number: {}, Message: {}", d1, d2);
+    }
+}
+
+#[derive(Debug)]
+enum Ack {
+    ExactlyOnce,
+    AtMostOnce,
+    AtLeastOnce
+}
+
+#[derive(Debug)]
+enum EnhancedAck {
+    ExactlyOnce,
+    AtMostOnce(String),
+    AtLeastOnce(i32, String)
 }
