3b26a0b9d002c98bfbca98a810eae36cc63e3b93
madlyrust <s.sivakumar77@hotmail.com>
2023-01-24 22:10:33 +0800

    Error handling example

--- expect/src/main.rs
+++ expect/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), String> {
+    // Declaring variable with Ok() variant of Result type
+    let a: Result<i32, &str> = Ok(100);
+
+    // expect() method panics with a provided custom message
+    println!("Value of variable a => {}", a.expect("Variable cannot be empty"));
+
+    // unwrap() method panics with a generic message
+    println!("Value of variable a => {}", a.unwrap());
+
+    // When there is no ? operator to propagate errors
+    let r = print_me(2);
+    if let Err(x) = r {
+        return Err(x)
+    }
+
+    let r = print_me(3);
+    if let Err(x) = r {
+        return Err(x)
+    }
+
+    // Using ? operator to propagate errors
+    print_me(100)?;
+    return Ok(())
+}
+
+fn print_me(n: i32) -> Result<i32, String> {
+    if n < 5 {
+        Ok(n)
+    } else {
+        Err(format!("Given input {n} is invalid!!!"))
+    }
 }
--- expect/target/debug/deps/expect-77f49bea46194b1e.d
+++
@@ -1,5 +0,0 @@
-c:\rust-examples\expect\target\debug\deps\expect-77f49bea46194b1e.rmeta: src\main.rs
-
-c:\rust-examples\expect\target\debug\deps\expect-77f49bea46194b1e.d: src\main.rs
-
-src\main.rs:
