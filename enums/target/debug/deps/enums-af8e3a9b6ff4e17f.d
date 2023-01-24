2c6fe357448e8608f975b16faf3b01cd1f3e2d22
madlyrust <s.sivakumar77@hotmail.com>
2023-01-24 15:11:39 +0800

    Deleted target

--- enums/target/debug/deps/enums-af8e3a9b6ff4e17f.d
+++
@@ -1,5 +0,0 @@
-c:\rust-examples\enums\target\debug\deps\enums-af8e3a9b6ff4e17f.rmeta: src\main.rs
-
-c:\rust-examples\enums\target\debug\deps\enums-af8e3a9b6ff4e17f.d: src\main.rs
-
-src\main.rs:
--- enums/target/debug/deps/enums-f29eecea98c5626a.d
+++
@@ -1,5 +0,0 @@
-c:\rust-examples\enums\target\debug\deps\enums-f29eecea98c5626a.rmeta: src\main.rs
-
-c:\rust-examples\enums\target\debug\deps\enums-f29eecea98c5626a.d: src\main.rs
-
-src\main.rs:
